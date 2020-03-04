b796710e3a278848cb211fe97e1fc13c037081ed
tianyalu <styzf@sina.com>
2020-03-05 01:26:37 +0800

    添加资源文件

--- app/src/main/java/com/sty/ne/ffmpegplayer/MainActivity.java
+++ app/src/main/java/com/sty/ne/ffmpegplayer/MainActivity.java
@@ -19,10 +19,11 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
     private static final String DIR_PATH = Environment.getExternalStorageDirectory()
-//            + File.separator + "sty" + File.separator + "input.mp4";
+            + File.separator + "sty" + File.separator + "input.mp4";
+//            + File.separator + "sty" + File.separator + "input_crop.mp4";
 //            + File.separator + "sty" + File.separator + "chengdu.mp4";
 //            + File.separator + "视频/dance/sandymandy" + File.separator + "[牛人]Whatcha Doin' Today_超清.mp4";
-            + File.separator + "视频/dance/sandymandy" + File.separator + "4Minute - Hate by Sandy Mandy_超清.mp4";
+//            + File.separator + "视频/dance/sandymandy" + File.separator + "4Minute - Hate by Sandy Mandy_超清.mp4";
     private String[] needPermissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     private static final int STATUS_STOP = 0;
